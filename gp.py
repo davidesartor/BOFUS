5e17bf21aa6ca561ed3ebc4aa0c2fe12a2ff0fd8
davidesartor <davide.sartor.4@studenti.unipd.it>
2026-03-16 15:22:57 -0400

    Stable EI function

--- gp.py
+++ gp.py
@@ -1,19 +1,18 @@
-from typing import NamedTuple, Optional
-from jaxtyping import Array, Float, Key
-from dataclasses import dataclass, field
+from typing import NamedTuple, Literal, Optional, Self
+from jaxtyping import Array, Float
 
 import jax
 import jax.numpy as jnp
 import jax.scipy as jsp
-import jax.random as jr
-from jax.numpy.linalg import norm
-from einops import rearrange
-import scipy
-import numpy
+import equinox as eqx
+import scipy as sp
+import numpy as np
 import autobounds
 
 EPS = float(jnp.sqrt(jnp.finfo(float).eps))
-MAX_NUGGET = 1e-1
+MAX_NUGGET = 1e-3
+
+Kernel = Literal["squared_exponential", "matern_5_2", "matern_3_2", "matern_1_2"]
 
 
 class Gaussian(NamedTuple):
@@ -21,42 +20,36 @@ class Gaussian(NamedTuple):
     cov: Float[Array, "n n"]
 
 
-class Parameters(NamedTuple):
-    theta: Float[Array, "o d"]
-    g: Float[Array, "o"]
-    b: Float[Array, "o"]
-    nu: Float[Array, "o"]
-
-
-def kernel(
+@eqx.filter_jit
+def cov_fn(
+    kernel: Kernel,
+    theta: Float[Array, "d"],
     x1: Float[Array, "n d"],
     x2: Float[Array, "m d"],
-    theta: Float[Array, "d"],
 ) -> Float[Array, "n m"]:
-    def squared_distance(
-        x1: Float[Array, "n d"],
-        x2: Float[Array, "m d"],
-    ) -> Float[Array, "n m"]:
-        def dist(a, b):
-            return jnp.sum((a - b) ** 2)
+    def eval_single(
+        a: Float[Array, "d"],
+        b: Float[Array, "d"],
+    ):
+        d2 = jnp.sum(jnp.square(a - b) / theta)
+        d = jnp.sqrt(jnp.where(d2 > 0, d2, EPS))
+        if kernel == "squared_exponential":
+            k = jnp.exp(-0.5 * d2)
+        elif kernel == "matern_1_2":
+            k = jnp.exp(-d)
+        elif kernel == "matern_3_2":
+            k = (1 + jnp.sqrt(3) * d) * jnp.exp(-jnp.sqrt(3) * d)
+        elif kernel == "matern_5_2":
+            k = (1 + jnp.sqrt(5) * d + 5 / 3 * d2) * jnp.exp(-jnp.sqrt(5) * d)
+        k = jax.lax.cond(d2 == 0.0, lambda: 1.0, lambda: k)
+        return k
 
-        dist = jax.vmap(jax.vmap(dist, (None, 0)), (0, None))
-        return dist(x1, x2)
-
-    # matern 5/2 kernel
-    theta = jnp.sqrt(theta)
-    d2 = squared_distance(x1 / theta, x2 / theta)
-    d = jnp.sqrt(d2 + EPS)
-    return (1 + jnp.sqrt(5) * d + 5 / 3 * d2) * jnp.exp(-jnp.sqrt(5) * d)
-
-    # squared exponential kernel
-    theta = jnp.sqrt(theta)
-    d2 = squared_distance(x1 / theta, x2 / theta)
-    return jnp.exp(-0.5 * d2)
+    return jax.vmap(jax.vmap(eval_single, (None, 0)), (0, None))(x1, x2)
 
 
-@jax.jit
+@eqx.filter_jit
 def likelihood(
+    kernel: Kernel,
     theta: Float[Array, "d"],
     g: Float[Array, ""],
     x: Float[Array, "n d"],
@@ -65,7 +58,7 @@ def likelihood(
     n, d = x.shape
 
     # foward of kernel
-    K = kernel(x, x, theta)
+    K = cov_fn(kernel, theta, x, x)
     K = K + jnp.eye(n) * (EPS + g)
 
     # cholesky of K and compute logdet
@@ -82,38 +75,34 @@ def likelihood(
     b = (Ki_1 * y).sum() / Ki_1.sum()
     nu = jnp.dot((y - b) / n, (Ki_y - Ki_1 * b))
 
-    # # likelihood when marginalizing over trend and variance
+    # likelihood when marginalizing over trend and variance
     loglik = -0.5 * (n * jnp.log(nu) + logdetK)
     return loglik, b, nu
 
 
-@jax.jit
-@jax.value_and_grad
-def mle_loss(
-    theta_g: Float[Array, "d+1"],
-    x_train: Float[Array, "n d"],
-    y_train: Float[Array, "n"],
-):
-    theta, g = theta_g[:-1], theta_g[-1]
-    loglik, b, nu = likelihood(theta, g, x_train, y_train)
-    return -loglik
-
-
 class GaussianProcess(NamedTuple):
+    kernel: Kernel
     x: Float[Array, "n d"]
     y: Float[Array, "n"]
-    parameters: Parameters
-    bounds: Float[Array, "2 o d"]
+    theta: Float[Array, "d"]
+    g: Float[Array, ""]
+    b: Float[Array, ""]
+    nu: Float[Array, ""]
+    Koo: Float[Array, "n n"]
 
     @classmethod
     def fit(
         cls,
         x: Float[Array, "n d"],
         y: Float[Array, "n"],
-        warmstart: Optional[Parameters] = None,
+        *,
+        warmstart: Optional[Self] = None,
+        kernel: Kernel = "squared_exponential",
         max_iterations: int = 100,
         verbose: bool = False,
-    ) -> "GaussianProcess":
+    ):
+        n, d = x.shape
+
         # initialization
         lower, upper = autobounds.hetgpy_auto_bounds(x)
         init_theta = 0.9 * upper + 0.1 * lower
@@ -139,11 +128,18 @@ class GaussianProcess(NamedTuple):
             print(f"Max: {bounds[1, ..., -1]}")
             print()
 
+        # define the loss function for optimization
+        @jax.jit
+        @jax.value_and_grad
+        def mle_loss(theta_g: Float[Array, "d+1"]):
+            theta, g = theta_g[:-1], theta_g[-1]
+            loglik, b, nu = likelihood(kernel, theta, g, x, y)
+            return -loglik
+
         # run optimization
-        result = scipy.optimize.minimize(
+        result = sp.optimize.minimize(
             fun=mle_loss,
             x0=jnp.concatenate([init_theta, init_g[None]]),
-            args=(x, y),
             jac=True,
             method="L-BFGS-B",
             bounds=[(a, b) for a, b in zip(*bounds)],
@@ -152,8 +148,8 @@ class GaussianProcess(NamedTuple):
 
         # extract the optimal parameters and infer the rest
         theta, g = result.x[..., :-1], result.x[..., -1]
-        llk, b, nu = likelihood(theta, g, x, y)
-
+        llk, b, nu = likelihood(kernel, theta, g, x, y)
+        Koo = nu * (cov_fn(kernel, theta, x, x) + jnp.eye(x.shape[0]) * (EPS + g))
         if verbose:
             print(f"Optimal theta: {theta}")
             print(f"Optimal g: {g}")
@@ -161,34 +157,52 @@ class GaussianProcess(NamedTuple):
             print(f"Optimal nu: {nu}")
             print(f"Log-likelihood at optimum: {llk}")
             print()
-        return cls(x=x, y=y, parameters=Parameters(theta, g, b, nu), bounds=bounds)
 
-    def update(self, x: Float[Array, "n d"], y: Float[Array, "n"]):
+        return GaussianProcess(
+            kernel=kernel, x=x, y=y, theta=theta, g=g, b=b, nu=nu, Koo=Koo
+        )
+
+    def update(
+        self,
+        x: Float[Array, "n d"],
+        y: Float[Array, "n"],
+        *,
+        warmstart: bool = True,
+        max_iterations: int = 100,
+        verbose: bool = False,
+    ):
         x = jnp.concatenate([self.x, x], axis=0)
         y = jnp.concatenate([self.y, y], axis=0)
-        return self.fit(x, y, warmstart=self.parameters)
+        return self.fit(
+            x,
+            y,
+            kernel=self.kernel,
+            warmstart=self if warmstart else None,
+            max_iterations=max_iterations,
+            verbose=verbose,
+        )
 
-    @jax.jit
+    @eqx.filter_jit
     def predict(self, x: Float[Array, "n d"]) -> Gaussian:
+        theta, g, b, nu = self.theta, self.g, self.b, self.nu
         n, d = self.x.shape
-        theta, g, b, nu = self.parameters
 
-        Koo = nu * (kernel(self.x, self.x, theta) + jnp.eye(n) * (EPS + g))
-        Kxo = nu * kernel(x, self.x, theta)
-        Kxx = nu * kernel(x, x, theta)
+        Kxo = nu * cov_fn(self.kernel, theta, x, self.x)
+        Kxx = nu * cov_fn(self.kernel, theta, x, x)
 
         # posterior mean and covariance
-        mean = b + Kxo @ jnp.linalg.solve(Koo, self.y - b)
-        cov = Kxx - Kxo @ jnp.linalg.solve(Koo, Kxo.T)
+        mean = self.b + Kxo @ jnp.linalg.solve(self.Koo, self.y - self.b)
+        cov = Kxx - Kxo @ jnp.linalg.solve(self.Koo, Kxo.T)
 
         # Add correction based on the trend estimation correlation
-        Kbx = jnp.ones((1, n)) @ jnp.linalg.solve(Koo, Kxo.T)
-        cov = cov + (1 - Kbx).T @ (1 - Kbx) / jnp.linalg.inv(Koo).sum()
+        Kbx = jnp.ones((1, n)) @ jnp.linalg.solve(self.Koo, Kxo.T)
+        cov = cov + (1 - Kbx).T @ (1 - Kbx) / jnp.linalg.inv(self.Koo).sum()
         return Gaussian(mean=mean, cov=cov)
 
-    @jax.jit
+    @eqx.filter_jit
     def expected_improvement(self, x: Float[Array, "n d"]) -> Float[Array, "n"]:
         # numerically stable version following https://arxiv.org/pdf/2310.20708:
+        # NOTE: it might fail due to numerical precision if the argument of log1mexp is negative
         mu, cov = self.predict(x)
         sigma = jnp.diag(cov) ** 0.5
         y_star = self.y.min()
@@ -206,44 +220,9 @@ class GaussianProcess(NamedTuple):
             branch2 = lambda: (
                 -(z**2) / 2
                 - jnp.log(2 * jnp.pi) / 2
-                + jax.lax.cond(z < -1 / EPS, branch2a, branch2b)
+                + jax.lax.cond(z < -1 / jnp.sqrt(EPS), branch2a, branch2b)
             )
             return jax.lax.cond(z > -1, branch1, branch2)
 
-        return jnp.log(sigma) + jax.vmap(eval_single)(z)
-
-    def acquisition(
-        self, multistart: int = 16, max_iterations=10, verbose: bool = False
-    ) -> tuple[Float[Array, "n d"], Float[Array, "n"]]:
-        @jax.jit
-        @jax.value_and_grad
-        def loss(x_flat):
-            x = x_flat.reshape(-1, d)
-            return -self.expected_improvement(x).sum()
-
-        def verbose_loss(x_flat):
-            v, g = loss(x_flat)
-            if jnp.isnan(v):
-                print(f"NaN loss at {x_flat}")
-            elif jnp.any(jnp.isnan(g)):
-                print(f"NaN in gradient at {x_flat}")
-            return v, g
-
-        n, d = self.x.shape
-        x0 = scipy.stats.qmc.LatinHypercube(d).random(n=multistart).flatten()
-        result = scipy.optimize.minimize(
-            fun=verbose_loss,
-            x0=x0,
-            jac=True,
-            method="L-BFGS-B",
-            bounds=[(0, 1) for _ in range(len(x0))],
-            options=dict(maxiter=max_iterations, ftol=EPS, gtol=0),
-        )
-        x = jnp.array(result.x.reshape(-1, d))
-        ei = self.expected_improvement(x)
-        best = x[jnp.argmax(ei)]
-        if verbose:
-            for i in range(len(x)):
-                print(f"Acquisition point {i}: {x[i]}, EI: {ei[i]}")
-            print(f"best: {best}, best EI: {jnp.max(ei)}")
-        return best, x
+        ei = jnp.log(sigma) + jax.vmap(eval_single)(z)
+        return ei
